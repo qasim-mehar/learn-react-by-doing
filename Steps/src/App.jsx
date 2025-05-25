564b62652f003b6be7ee7ef813a708374580e528
Qasim <mianqasim065@gmail.com>
2025-05-25 13:19:22 +0500

    Add isOpen useState and JSX fragment to re-rendor component conditionally

--- Steps/src/App.jsx
+++ Steps/src/App.jsx
@@ -8,6 +8,7 @@ const messages = [
 ];
 function App() {
   const [step,setStep]=useState(1);
+  const [isOpen,setOpen]=useState(true);
   // let step=1;
   function stepNext() {
     if(step<3){
@@ -21,14 +22,17 @@ function App() {
     }
   }
   return(
+    <>
+      <button onClick={()=>setOpen(!isOpen)} className='close'>&times;</button>
+    {isOpen && (
     <div className='steps'>
       <div className="numbers">
-       <div className={`${step>=1?"active":""}`}>1</div>
-       <div className={`${step>=2?"active":""}`}>2</div>
-       <div className={`${step>=3?"active":""}`}>3</div>
+       <div className={`${step >= 1 ?"active":""}`}>1</div>
+       <div className={`${step >= 2 ?"active":""}`}>2</div>
+       <div className={`${step >= 3 ?"active":""}`}>3</div>
       </div>
       <p className="message">
-         {step}:{messages[step-1]}
+         Step{step}:{messages[step-1]}
       </p>
       <div className="buttons">
       <button style={{backgroundColor:"#7950f2", color:"#fff"}} 
@@ -42,6 +46,8 @@ function App() {
 
       </div>
     </div>
+    )}
+    </>
   )
   
 }
