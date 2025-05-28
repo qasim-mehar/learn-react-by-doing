192d87919a8f5662c994ff78cdb60d5265db7538
Qasim <mianqasim065@gmail.com>
2025-05-28 21:38:51 +0500

    Feat: Created Form Componenet in triptote project

--- TripTote/src/App.jsx
+++ TripTote/src/App.jsx
@@ -31,81 +31,41 @@ function Logo(){
     
   )
 }
+
+
 function Form() {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(quantity)
     console.log(description);
-    if (!description) return;
-
-    // const newItem = { description, quantity, packed: false, id: Date.now() };
-
-    // onAddItems(newItem);
-
-    setDescription("");
-    setQuantity(1);
+    console.log(quantity);
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for your 😍 trip?</h3>
-      <select
-        value={quantity}
-        onChange={(e) => setQuantity(Number(e.target.value))}
-      >
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num} key={num}>
-            {num}
+      <h3>What do you want to add?</h3>
+
+      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
+        {Array.from({ length: 10 }, (_, i) => (
+          <option value={i + 1} key={i + 1}>
+            {i + 1}
           </option>
         ))}
       </select>
+
       <input
         type="text"
         placeholder="Item..."
         value={description}
         onChange={(e) => setDescription(e.target.value)}
       />
-      <button>Add</button>
+
+      <button type="submit">Add</button>
     </form>
   );
 }
-
-// function Form() {
-//   const [description, setDescription] = useState("");
-//   const [quantity, setQuantity] = useState(1);
-
-//   function handleSubmit(e) {
-//     e.preventDefault();
-//     console.log(description);
-//     console.log(quantity);
-//   }
-
-//   return (
-//     <form className="add-form" onSubmit={handleSubmit}>
-//       <h3>What do you want to add?</h3>
-
-//       <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
-//         {Array.from({ length: 10 }, (_, i) => (
-//           <option value={i + 1} key={i + 1}>
-//             {i + 1}
-//           </option>
-//         ))}
-//       </select>
-
-//       <input
-//         type="text"
-//         placeholder="Item..."
-//         value={description}
-//         onChange={(e) => setDescription(e.target.value)}
-//       />
-
-//       <button type="submit">Add</button>
-//     </form>
-//   );
-// }
 
 function PackingList(){
   return(
