81496885c8511c7f18ac536a1d07e3c0a83fbf6d
Qasim <mianqasim065@gmail.com>
2025-05-28 18:28:46 +0500

    Created List and PackingList Components

--- TripTote/src/App.jsx
+++ TripTote/src/App.jsx
@@ -1,31 +1,139 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 
+// import { useState } from 'react'
+import { useState } from 'react';
 import './index.css'
 
-function App(){}
-function logo(){
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 2, packed: false }
+];
+
+
+function App(){
+  // const [items,setItem]=useState(0);
+ 
+ return (
+ <>
+           <Logo/>
+          <Form/>
+          <PackingList/>
+          <Stats />
+ </>
+        
+ )
+}
+function Logo(){
   return(
     <h1>Far Away</h1>
+    
   )
 }
-function Form(){
-  return(
-    <div className="add-form">
-        <h3>What do you want to add?</h3>
-    </div>
-  )
+function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(quantity)
+    console.log(description);
+    if (!description) return;
+
+    // const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    // onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
 }
+
+// function Form() {
+//   const [description, setDescription] = useState("");
+//   const [quantity, setQuantity] = useState(1);
+
+//   function handleSubmit(e) {
+//     e.preventDefault();
+//     console.log(description);
+//     console.log(quantity);
+//   }
+
+//   return (
+//     <form className="add-form" onSubmit={handleSubmit}>
+//       <h3>What do you want to add?</h3>
+
+//       <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
+//         {Array.from({ length: 10 }, (_, i) => (
+//           <option value={i + 1} key={i + 1}>
+//             {i + 1}
+//           </option>
+//         ))}
+//       </select>
+
+//       <input
+//         type="text"
+//         placeholder="Item..."
+//         value={description}
+//         onChange={(e) => setDescription(e.target.value)}
+//       />
+
+//       <button type="submit">Add</button>
+//     </form>
+//   );
+// }
+
 function PackingList(){
   return(
-    <div className="list">
-      List
-    </div>
+  
+       <ul className="list">
+        {initialItems.map(item=> <List key={item.id} item={item} />)}
+        {/* {initialItems.map(item=> console.log(item))} */}
+       </ul>
+    
   )
 }
-function Stats(){
+
+function List({item}){ //we simply accepting a an item object inside an object so we simply  destructured here in the bracket notation
+  const{id,description,quantity,packed}=item; //we further destructured it so we can simply use values cleanly.
+return(
+  <li   >
+      <button>❌</button>
+      <span style={packed?{textDecoration: "line-through"}:{}}>{quantity} <strong>{description}</strong></span>
+       
+      <input type="checkbox" name="check" id={id} />
+  </li>
+)
+}
+function Stats(props){
+  // console.log(props)
   return(
     <footer>
-      you have X items and You already packed X
+      {`you have ${props.items} items and You already packed X`}
     </footer>
   )
 }
