f0482ab6b0770042e05c59ee97ea9edab3c845f1
Qasim <mianqasim065@gmail.com>
2025-05-31 16:34:32 +0500

    Feat:  Add sorting feature in triptote project

--- TripTote/src/App.jsx
+++ TripTote/src/App.jsx
@@ -90,13 +90,27 @@ function Form({onAddItem}) {
 }
 
 function PackingList({items,onDeleteItem,onPackedItem}){
-  
+  const [sortBy, setSortBy]=useState("value")
+  let sortedItem;
+  function sortItem(sortBy){
+     if (sortBy=="value") sortedItem=items;
+     if(sortBy=="description") sortedItem=items.slice().sort((a,b )=>a.description.localeCompare(b.description));
+     if(sortBy=="packed") sortedItem=items.slice().sort((a,b)=>Number(a.packed)-Number(b.packed));
+  }
+  sortItem(sortBy);
   return(
         <div className="list">
           <ul >
-             {items.map(item=> <List key={item.id} item={item} onDeleteItem={onDeleteItem} onPackedItem={onPackedItem}/>)}
+             {sortedItem.map(item=> <List key={item.id} item={item} onDeleteItem={onDeleteItem} onPackedItem={onPackedItem}/>)}
             
           </ul>
+          <div className="actions">
+            <select value={sortBy} onChange={(e)=>setSortBy(e.target.value)}>
+              <option value="value">sort by value</option>
+              <option value="description">sort by description</option>
+              <option value="packed">sort by packed</option>
+            </select>
+          </div>
         </div>
     
   )
