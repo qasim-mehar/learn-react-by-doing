e738116c1d63d4b6c3a00ac3a1704add211911f0
Qasim <mianqasim065@gmail.com>
2025-07-01 21:03:07 +0500

    Create bill + tip invoive at the buttom  in tip-counter project

--- tip-counter/src/App.jsx
+++ tip-counter/src/App.jsx
@@ -6,12 +6,16 @@ function App() {
   const [netBill, setNetBill] = useState(" ");
   const [userTip, setUserTip] = useState(" ");
   const [friendTip, setFriendTip] = useState(" ");
+  let netTip=(userTip+friendTip)/2;
    return(
    <>
      <BillField handleBill={setNetBill}/>
      <br />
      <Tip handleTip={setUserTip}>How much did you like the service? </Tip>
+     <br />
      <Tip handleTip={setFriendTip}>How much did Your friend like the service? </Tip>
+     <br />
+     <BillInvoice netBill={netBill} netTip={netTip}/>
    </>
    )
   
@@ -38,6 +42,11 @@ function Tip({handleTip,children}){
       <option value="20">Great 20%</option>
      </select>
     </>
+  )
+}
+function BillInvoice({netBill, netTip}){
+  return(
+    netBill? <h2>{`Your total bill is ${netBill}$ (${netBill} $ +  ${netTip} $ Tip)`}</h2> : <p>No bill so far</p>
   )
 }
 export default App
