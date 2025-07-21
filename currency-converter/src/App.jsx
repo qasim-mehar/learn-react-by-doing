ba969564e8d253176f6275ddf9926f4d3035a302
Qasim <mianqasim065@gmail.com>
2025-07-21 05:33:37 +0500

    Handle text fields data using states in currency-converter project

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [currency, setCurrency]=useState(1);
+  const [baseCurrencyType, setBaseCurrencyType]=useState(" ")
+  const [toConvertInCurrencyType, setToConvertInCurrencyType]=useState(" ")
+  
+  function handelBaseValue(e){
+    setCurrency(e.target.value)
+  }
+  function handleBaseCurrencyType(e){
+    setBaseCurrencyType(e.target.value)
+  }
+  function handleToConvertType(e){
+    setToConvertInCurrencyType(e.target.value);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <input value={currency} onChange={handelBaseValue} type="text" />
+      <select value={baseCurrencyType} onChange={handleBaseCurrencyType}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select value={toConvertInCurrencyType} onChange={handleToConvertType}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>OUTPUT</p>
+    </div>
+  );
 }
 
 export default App
