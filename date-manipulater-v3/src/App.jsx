373c3aa8583d5fad9defc245014e86f640fbd19c
Qasim <mianqasim065@gmail.com>
2025-07-26 04:38:23 +0500

    Use a single useReducer hook to manage count and step states and also use a switch statement to determined the type of action needed to update the satate

--- date-manipulater-v3/src/App.jsx
+++ date-manipulater-v3/src/App.jsx
@@ -1,18 +1,34 @@
-import {  useReducer, useState } from "react";
+import {  useReducer } from "react";
 import "./App.css";
+ const initialState={
+    count:0,
+    step: 1
+  }
 function reducer(state,action){
-  if(action.type==="inc")return state+1;
-  if(action.type==="dec")return state-1;
-  if(action.type==="setCount") return action.payload;
+  // if(action.type==="inc")return state+1;
+  // if(action.type==="dec")return state-1;
+  // if(action.type==="setCount") return action.payload;
+  switch (action.type) {
+    case "inc": return {...state, count: state.count+state.step}
+    case "dec": return{...state, count:state.count-state.step}
+    case "setCount" : return {...state, count:action.payload}
+    case "setStep": return{...state, step:action.payload}
+    case "reset": return initialState
+    default:
+      throw new Error("Unknown action");
+  }
   
 }
 function DateCounter() {
   // const [count, setCount] = useState(0);
-  const [count, dispatch]=useReducer(reducer,0);
-  const [step, setStep] = useState(1);
-
+  // const [count, dispatch]=useReducer(reducer,0);
+  // const [step, setStep] = useState(1);
+  
+ 
+  const [state, dispatch]=useReducer(reducer,initialState)
+  const {count,step}=state;
   // This mutates the date object.
-  const date = new Date("june 21 2027");
+  const date = new Date("july 26 2025");
   date.setDate(date.getDate() + count);
 
   const dec = function () {
@@ -33,13 +49,14 @@ function DateCounter() {
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    // setStep(Number(e.target.value));
+    dispatch({type:"setStep", payload:Number(e.target.value)})
   };
 
   const reset = function () {
     // setCount(0);
-    dispatch({type:"setCount", payload:0})
-    setStep(1);
+    dispatch({type:"reset"})
+    // setStep(1);
   };
 
   return (
