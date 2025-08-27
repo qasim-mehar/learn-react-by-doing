cce04d834ed7140c072003ff681afeca6c380d1a
Qasim <mianqasim065@gmail.com>
2025-08-27 19:28:56 +0500

    Refactor: Create feature folder and seprate customer and account components

---
+++ redux-intro/src/features/customer/Customer.jsx
@@ -0,0 +1,10 @@
+// import { useSelector } from "react-redux";
+// import { store } from "../../Store";
+
+// const customer=useSelector(store=>store.customer);
+function Customer() {
+  // console.log(customer);
+  return <h2>👋 Welcome, %NAME%</h2>;
+}
+
+export default Customer;
