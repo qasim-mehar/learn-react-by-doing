3cbec9f042ee6deeef5efcad2805ef8317fa26f2
Qasim <mianqasim065@gmail.com>
2025-08-27 19:56:56 +0500

    Feat: Conditionally render components

--- redux-intro/src/App.jsx
+++ redux-intro/src/App.jsx
@@ -2,16 +2,22 @@ import Customer from "./features/customer/Customer";
 import AccountOperations from "./features/account/AccountOperations"
 import CreateCustomer from "./features/customer/CreateCustomer";
 import BalanceDisplay from "./features/account/BalanceDisplay"
+import { useSelector } from "react-redux";
+
 
 
 function App() {
+  const customerName=useSelector(state=>state.customer.fullName)
+  console.log(customerName);
   return (
     <div>
       <h1>🏦 The React-Redux Bank ⚛️</h1>
-      <CreateCustomer />
-      <Customer />
+      {!customerName? (<CreateCustomer />):
+     (<>
+     <Customer />
       <AccountOperations />
       <BalanceDisplay />
+     </>)}
       
     </div>
   );
