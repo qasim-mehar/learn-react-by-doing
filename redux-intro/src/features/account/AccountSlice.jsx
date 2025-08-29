675e418bb8eeb1c2deb267bf8235b191bc4f3377
Qasim <mianqasim065@gmail.com>
2025-08-30 00:15:36 +0500

    Refactor: Convert currency as well using redux devtoolkit

--- redux-intro/src/features/account/AccountSlice.jsx
+++ redux-intro/src/features/account/AccountSlice.jsx
@@ -16,7 +16,7 @@ const accountSlice=createSlice({
    withdraw(state,action){
     state.balance-=action.payload;
    },
-
+   // Use prepare() to pass multiple arguments as one payload object
    requestLoan:{
     prepare(amount,purpose){
       return{
@@ -32,17 +32,32 @@ const accountSlice=createSlice({
       state.loan=action.payload.amount;
     }
    },
-   payLoan(state,action){
+   payLoan(state){
     state.balance-=state.loan;
     state.loan=0;
     state.purpose=""
+   },
+   convertingCurrency(state){
+    state.isLoading=true;
    }
   }
 
 })
 
-export const {deposit,withdraw,requestLoan,payLoan}=accountSlice.actions
+export const {withdraw,requestLoan,payLoan}=accountSlice.actions
 export default accountSlice.reducer
+export function deposit(amount,currency) {
+  if(currency=='USD') return { type: "account/deposit", payload:amount};
+  return async function(dispatch,getState){
+    dispatch({type:"account/convertingCurrency"})
+    const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`)
+    console.log(res);
+    const data=await res.json();
+    const converted =data.rates.USD;
+    console.log(converted);
+    dispatch({ type: "account/deposit", payload:converted })
+  }
+}
 // export function accountReducer(state = accountInitialState, action) {
 //   switch (action.type) {
 //     case "account/deposit":
@@ -87,19 +102,7 @@ export default accountSlice.reducer
 // }
 
 // // Action creators
-// export function deposit(amount,currency) {
-//   if(currency=='USD') return { type: "account/deposit", payload:amount};
 
-//   return async function(dispatch,getState){
-//     dispatch({type:"account/convertingCurrency"})
-//     const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`)
-//     console.log(res);
-//     const data=await res.json();
-//     const converted =data.rates.USD;
-//     console.log(converted);
-//     dispatch({ type: "account/deposit", payload:converted })
-//   }
-// }
 
 // export function withdraw(amount) {
 //   return { type: "account/withdraw", payload: { amount } };
